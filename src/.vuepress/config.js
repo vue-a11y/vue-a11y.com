2eaabd57788bf2e27a88066acd8b8aaea64e5a78
Alan Ktquez <ktquez@gmail.com>
2020-08-18 10:54:14 -0300

    Add config files

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,8 +1,27 @@
+require('dotenv').config()
+const webpack = require('webpack')
+
 const head = require('./config/head')
+const plugins = require('./config/plugins')
+const postcss = require('./config/postcss')
+const themeConfig = require('./config/themeConfig')
 
 module.exports = {
+  base: '/',
   title: 'Vue a11y',
   description: 'Vue.js community project to improve web accessibility.',
-  base: '/',
-  head
+  evergreen: true,
+  serviceWorker: true,
+  head,
+  plugins,
+  postcss,
+  locales,
+  themeConfig,
+  configureWebpack () {
+    return {
+      plugins: [
+        new webpack.EnvironmentPlugin({ ...process.env })
+      ],
+    }
+  }
 }
