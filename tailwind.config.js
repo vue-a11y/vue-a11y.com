3427d8b61979dec44ca49ec0a1ae34062ea23e4c
Alan Ktquez <ktquez@gmail.com>
2020-08-19 10:27:30 -0300

    Setting the theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
 module.exports = {
   purge: false,
+  future: {
+    removeDeprecatedGapUtilities: true
+  },
   theme: {
     extend: {
-      colors: {}
-    },
+      colors: {
+        accent: '#41B883',
+        light: {
+          200: '#FBFCFC',
+          400: '#EEF5F5'
+        },
+        sepia: {
+          text: '#473742',
+          200: '#F1E7D0',
+          400: '#EBDDBF',
+          600: '#E3D2AC'
+        },
+        dark: {
+          500: '#2E3134',
+          700: '#212529',
+          900: '#12181E'
+        }
+      }
+    }
   },
   variants: {},
-  plugins: [],
+  plugins: []
 }
