552139f5c0c74695e13a2d74910552c48ce4c8b7
Alan Ktquez <ktquez@gmail.com>
2020-08-20 11:16:34 -0300

    Add TheNavigation component

---
+++ src/.vuepress/theme/components/TheNavigation.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav aria-label="Main">
+    <ul class="flex">
+      <li
+        v-for="item in $themeLocaleConfig.nav"
+        :key="item.text"
+      >
+        <a
+          :href="item.link"
+          class="px-3 py-3 border-b-4 border-transparent border-solid hover:border-accent lg:py-4"
+        >
+          {{ item.text }}
+        </a>
+      </li>
+      <li>
+        <a
+          :href="`https://github.com/${$themeConfig.repo}`"
+          target="_blank"
+          rel="noopener"
+          class="px-3 py-5 ml-1 border-b-4 border-transparent border-solid hover:border-accent lg:py-4"
+        >
+          <span>Github</span>
+          <span class="sr-only">({{ $localeConfig.externalLinkText }})</span>
+          <OutboundLink />
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'TheNavigation'
+}
+</script>
