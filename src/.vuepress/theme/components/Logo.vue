9f35e53ddd98c76a4d023970d19edbf690c537cd
Alan Ktquez <ktquez@gmail.com>
2020-08-20 23:26:54 -0300

    Update Logo component

--- src/.vuepress/theme/components/Logo.vue
+++ src/.vuepress/theme/components/Logo.vue
@@ -3,67 +3,10 @@
     href="/"
     class="inline-flex p-3"
   >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="39.6"
-      height="33"
-      aria-hidden="true"
+    <img
+      src="/vue-a11y-logo.svg"
+      alt="Vue A11y logo"
     >
-      <title>Vue A11y</title>
-      <g transform="translate(5.1 5.3)">
-        <path
-          d="M0 3.8l14.2-.3S10.9 0 7 0 0 3.8 0 3.8zm0 0l14.2-.3s-3.1 3.7-7 3.8S0 3.8 0 3.8z"
-          fill="#d6d8da"
-          fill-rule="evenodd"
-        />
-        <circle
-          cx="2.5"
-          cy="2.5"
-          r="2.5"
-          transform="translate(4.6 1.3)"
-          fill="#3ab982"
-          opacity=".6"
-        />
-        <circle
-          cx="1"
-          cy="1"
-          r="1"
-          transform="translate(6.2 2.7)"
-          fill="#34495f"
-        />
-        <circle
-          cx=".5"
-          cy=".5"
-          r=".5"
-          transform="translate(7.3 2.4)"
-          fill="#3ab982"
-          opacity=".8"
-        />
-      </g>
-      <path
-        d="M30.8 32h-7l-4.5-7.5-4.2 7.5H.8L19.4 0l18.5 32zM19.4 12.7z"
-        fill="#3ab982"
-      />
-      <path
-        d="M23.7 32l-4.3-7.4-4.3 7.5H7.9l11.5-19.4 11.4 19.4z"
-        fill="#34495f"
-      />
-      <g
-        fill="#34495f"
-        fill-rule="evenodd"
-      >
-        <path d="M25.3 4A.6.6 0 0025 5a3.4 3.4 0 012.4 4.2.6.6 0 001.1.3A4.6 4.6 0 0025.3 4z" />
-        <path d="M26 1.8a.6.6 0 00-.3 1.2 5.7 5.7 0 014 7 .6.6 0 001.1.2A6.8 6.8 0 0026 1.8z" />
-      </g>
-      <path
-        d="M39.5 26a.5.5 0 00-.5-.3 2.2 2.2 0 00-1.1.4 3.8 3.8 0 00-1.2 1.3.5.5 0 01-.3.2v-5.1a.9.9 0 10-1.7 0v3.4a.1.1 0 01-.3 0v-5a.9.9 0 10-1.7 0v5a.1.1 0 01-.2 0v-4a.8.8 0 00-1.6 0v4.4a.1.1 0 01-.2 0v-2.7a.8.8 0 00-1.5 0v6c.2 3 2 3.4 3.9 3.4a4 4 0 003.5-2l.6-.9 1.3-2.1a2.7 2.7 0 01.6-.8 1.7 1.7 0 00.4-.6.9.9 0 000-.6z"
-        fill="#34495f"
-        fill-rule="evenodd"
-      /><path
-        d="M0 25.9l.4 4.3a.8.8 0 001.7-.1l-.3-3.4h1.1a4.3 4.3 0 00.8 5l1-1.2a2.8 2.8 0 014-3.9l1-1a4.3 4.3 0 00-2.2-1.2l-2-2.3 1.1-.7L8 22.6a.8.8 0 001.2-1.2l-2-1.7a.9.9 0 00-1 0l-3.8 2.2a1 1 0 00-.2 1.4L4 25.1l-3-.1a.8.8 0 00-.9.9zm1.8-4.3a1.4 1.4 0 111.4-1.4 1.4 1.4 0 01-1.4 1.4zm5 9.8a2.8 2.8 0 002.3-4.3l1.1-1a4.2 4.2 0 011 2.6 4.3 4.3 0 01-7 3.4l1-1.1a2.8 2.8 0 001.6.4z"
-        fill="#34495f"
-      />
-    </svg>
     <span class="font-bold sr-only text-22 lg:ml-2 lg:not-sr-only ">Vue A11y</span>
   </a>
 </template>
