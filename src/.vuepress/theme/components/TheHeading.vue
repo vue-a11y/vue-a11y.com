ea62fe909a9f3e109a9da90e13c4d05d1d28263f
Alan Ktquez <ktquez@gmail.com>
2020-08-25 11:59:02 -0300

    fix: watch route path to event toggle-sidebar

--- src/.vuepress/theme/components/TheHeading.vue
+++ src/.vuepress/theme/components/TheHeading.vue
@@ -75,7 +75,7 @@ export default {
     const labels = root.$themeLocaleConfig.a11y.labels
     const menuButtonAriaLabel = labels.menuButton ? computed(() => props.isSidebarOpen ? labels.menuButton.close : labels.menuButton.open) : 'Menu sidebar button'
 
-    watch(() => root.$route, () => emit('toggle-sidebar'))
+    watch(() => root.$route.path, () => emit('toggle-sidebar'))
 
     return {
       menuButtonAriaLabel
