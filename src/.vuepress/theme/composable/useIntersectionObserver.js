1b4aef3ebdbf8919bb6073fda3f718be4f711feb
Alan Ktquez <ktquez@gmail.com>
2020-08-25 10:18:28 -0300

    Add useIntersectionObserver

---
+++ src/.vuepress/theme/composable/useIntersectionObserver.js
@@ -0,0 +1,52 @@
+import { onMounted, ref, onUnmounted } from '@vue/composition-api'
+
+export default function useIntersectionObserver (selector, options) {
+  const intersectionRatio = ref(0)
+  const isIntersecting = ref(false)
+  const isFullyInView = ref(false)
+  const targetIntercepted = ref(null)
+  const targets = ref(null)
+
+  const defaultOptions = { root: null, rootMargin: '0px' }
+
+  function observe () {
+    targets.value.forEach(el => observer.observe(el))
+  }
+
+  let observer
+  onMounted(() => {
+    if ('IntersectionObserver' in window) {
+      targets.value = document.querySelectorAll(selector)
+      if (!targets.value) return
+      observer = new IntersectionObserver(([entry]) => {
+        intersectionRatio.value = entry.intersectionRatio
+        if (entry.intersectionRatio > 0) {
+          targetIntercepted.value = entry.target
+          isIntersecting.value = true
+          isFullyInView.value = entry.intersectionRatio >= 1
+          return
+        }
+
+        isIntersecting.value = false
+      }, { ...defaultOptions, ...options })
+
+      observe()
+    }
+  })
+
+  function unobserve () {
+    if (!observer) return
+    targets.value.forEach(el => observer.unobserve(el))
+  }
+
+  onUnmounted(unobserve)
+
+  return {
+    intersectionRatio,
+    isIntersecting,
+    isFullyInView,
+    targetIntercepted,
+    observe,
+    unobserve
+  }
+}
