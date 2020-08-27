eed922c167e2f277f393609ac789504d5b7ad3de
Alan Ktquez <ktquez@gmail.com>
2020-08-26 21:28:48 -0300

    Add authors data in themeConfig

---
+++ src/.vuepress/config/authors.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    name: 'Alan Ktquez',
+    username: 'ktquez',
+    website: 'https://github.com/ktquez',
+    social: [
+      {
+        name: 'twitter',
+        username: 'ktquez'
+      }
+    ],
+    thumb: 'https://avatars0.githubusercontent.com/u/8084606?s=460&u=20b6499a416cf7129a18e5c168cf387e159edb1a&v=4',
+    bio: 'Front-end developer • Vue.js • HTML & A11y enthusiast. Created of @vue-a11y and @htmlmoderno.'
+  }
+]
--- src/.vuepress/config/themeConfig.js
+++ src/.vuepress/config/themeConfig.js
@@ -5,5 +5,6 @@ module.exports = {
   editLinks: true,
   lastUpdated: true,
   searchPlaceholder: 'Search (Press "s" to focus)',
+  authors: require('./authors'),
   locales: require('./translations/themeLocaleConfig')
 }
