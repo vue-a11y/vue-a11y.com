2032406c19d83118a5c3cce0ca6c50bf36fc458f
Alan Ktquez <ktquez@gmail.com>
2020-08-27 18:55:20 -0300

    Add ShareLinks component

---
+++ src/.vuepress/theme/components/ShareLinks.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="page-page-share toc">
+    <span class="toc-title">Share</span>
+    <ul class="toc-list">
+      <li
+        v-for="(item, index) in shareLinks"
+        :key="`share-item-${index}`"
+        class="toc-list-item"
+      >
+        <ExternalLink
+          :href="item.href"
+          :label="item.label"
+        />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed } from '@vue/composition-api'
+
+import { prepareSocialShareLinks } from '@/theme/utils'
+
+export default {
+  name: 'ShareLinks',
+
+  props: {
+    path: {
+      type: String,
+      required: true
+    },
+
+    title: {
+      type: String,
+      required: true
+    },
+
+    summary: {
+      type: String,
+      default: ''
+    }
+  },
+
+  setup (props) {
+    const shareLinks = computed(() => (prepareSocialShareLinks(props.path, props.title, props.summary)))
+
+    return {
+      shareLinks
+    }
+  }
+}
+</script>
