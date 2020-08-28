dbb5172da1644d12853aee9d1f2e56874991869f
Alan Ktquez <ktquez@gmail.com>
2020-08-28 11:26:06 -0300

    Add composable useWindowSize

--- src/.vuepress/theme/composable/index.js
+++ src/.vuepress/theme/composable/index.js
@@ -1,3 +1,4 @@
 export { default as useSidebar } from './useSidebar'
+export { default as useWindowSize } from './useWindowSize'
 export { default as useEventListener } from './useEventListener'
 export { default as useIntersectionObserver } from './useIntersectionObserver'
