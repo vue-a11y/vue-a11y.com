742c837b45225f6a4a69230b233159ab2f655636
Alan Ktquez <ktquez@gmail.com>
2020-09-04 06:34:02 -0300

    Add tips inside recipes dir

--- src/.vuepress/config/translations/themeLocaleConfig/sidebarStructures.js
+++ src/.vuepress/config/translations/themeLocaleConfig/sidebarStructures.js
@@ -93,7 +93,7 @@ module.exports = {
       title: 'Tips',
       type: 'manual',
       children: [
-        ['ARIA attributes', '/tips/aria-attributes/']
+        'tips/aria-attributes'
       ]
     }
   ]
