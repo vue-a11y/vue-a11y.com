8013f92be8926fb0c1996aa6fd549ca8577677cd
Marcus Herrmann <mail@marcus-herrmann.com>
2020-10-16 07:59:16 +0200

    (post) Vue announcer: Adding MH to authors

--- src/.vuepress/config/authors.js
+++ src/.vuepress/config/authors.js
@@ -11,5 +11,18 @@ module.exports = [
     ],
     avatar: '/img/authors/ktquez.png',
     bio: 'Front-end developer • Vue.js • HTML & A11y enthusiast. Created of @vue-a11y and @htmlmoderno.'
+  },
+  {
+    name: 'Marcus Herrmann',
+    username: 'marcus-herrmann',
+    website: 'https://marcus.io',
+    social: [
+      {
+        name: 'twitter',
+        username: '_marcusherrmann'
+      }
+    ],
+    avatar: '/img/authors/mh.png',
+    bio: 'Freelance web developer and IAAP-certified Web Accessibility Specialist. Helping hand @tournantdev and @vue-a11y'
   }
 ]
