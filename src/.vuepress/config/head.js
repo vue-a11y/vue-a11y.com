829ee8e546298caf2d5b00a9dbb41d84cc06dcf9
yamanoku <0910yama@gmail.com>
2021-01-24 12:13:17 +0900

    fix: og:image full path

--- src/.vuepress/config/head.js
+++ src/.vuepress/config/head.js
@@ -24,7 +24,7 @@ module.exports = [
   // Facebook
   ['meta', { property: 'og:site_name', content: 'Vue A11y' }],
   ['meta', { property: 'og:type', content: 'website' }],
-  ['meta', { property: 'og:image', content: '/img/social-share.png' }],
+  ['meta', { property: 'og:image', content: 'https://vue-a11y.com/img/social-share.png' }],
   ['meta', { property: 'og:image:type', content: 'image/png' }],
   ['meta', { property: 'og:image:width', content: '1200' }],
   ['meta', { property: 'og:image:height', content: '360' }],
